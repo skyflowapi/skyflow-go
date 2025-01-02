af46444692f62f9f232737719c7c9cbf421fd6da
skyflow-bharti <bharti.sagar@skyflow.com>
2025-01-02 23:26:05 +0530

    SK-1780 fix invoke connection sample

--- samples/v2/vaultapi/invoke_connection.go
+++ samples/v2/vaultapi/invoke_connection.go
@@ -3,42 +3,61 @@ package vaultapi
 import (
 	"context"
 	"fmt"
-	. "skyflow-go/v2/client"
-	. "skyflow-go/v2/utils/common"
-	"skyflow-go/v2/utils/logger"
+	"github.com/skyflowapi/skyflow-go/v2/utils/logger"
+
+	. "github.com/skyflowapi/skyflow-go/v2/client"
+	. "github.com/skyflowapi/skyflow-go/v2/utils/common"
 )
 
 func main() {
+	// Add connection configurations 1
+	connConfig1 := ConnectionConfig{ConnectionId: "<CONNECTION_ID1>", ConnectionUrl: "<CONNECTION_URL1>", Credentials: Credentials{Token: "<BEARER_TOKEN1>"}}
 
-	// Connection 1 configuration
-	connConfig1 := ConnectionConfig{ConnectionId: "<CONNECTION_ID_1>", ConnectionUrl: "<CONNECTION_ID_URL>", Credentials: Credentials{Token: "<TOKEN>"}}
+	// Add connection configurations 2
+	connConfig2 := ConnectionConfig{ConnectionId: "<CONNECTION_ID2>", ConnectionUrl: "<CONNECTION_URL2>", Credentials: Credentials{Token: "<BEARER_TOKEN2>"}}
 
-	// Connection 2 configuration
-	connConfig2 := ConnectionConfig{
-		ConnectionId:  "<CONNECTION_ID_2>",
-		ConnectionUrl: "<CONNECTION_URL_2>",
-		Credentials: Credentials{
-			Token: "<TOKEN>",
-		},
-	}
-	skyflow1 := Skyflow{}
-	client1, _ := skyflow1.Builder().WithConnectionConfig(connConfig1).WithConnectionConfig(connConfig2).WithLogLevel(logger.DEBUG).Build()
-	service, _ := client1.Connection("<CONNECTION_ID>")
-	ctx := context.TODO()
-	res, getErr := service.Invoke(ctx, InvokeConnectionRequest{
-		Method: POST, // set the request method
-		Headers: map[string]string{
-			"Content-Type": "application/json", // Set the content type
-		},
-		Body: map[string]interface{}{ // Set your data
-			"<KEY_1>": "<VALUE_1>",
-			"<KEY_2>": "<VALUE_2>",
-		},
-	})
-	if getErr != nil {
-		fmt.Println("ERROR: ", *getErr)
+	var arr []ConnectionConfig
+	arr = append(arr, connConfig1, connConfig2)
+
+	// Initialize Skyflow client
+	client1, clientError := NewSkyflow(
+		WithConnections(arr...),
+		WithLogLevel(logger.DEBUG),
+	)
+	if clientError != nil {
+		fmt.Println("Error:", clientError)
 	} else {
-		fmt.Println("RESPONSE", res)
+		service, conError := client1.Connection("<CONNECTION_ID1>")
+		if conError != nil {
+			fmt.Println("Error:", conError)
+		} else {
+			ctx := context.TODO()
+			body := map[string]interface{}{ // Set your data
+				"<HEADER_NAME_1>": "<HEADER_VALUE_1>",
+				"<HEADER_NAME_2>": "<HEADER_VALUE_2>",
+			}
+			headers := map[string]string{
+				"Content-Type": "application/json", // Set the content type
+			}
+			queryParams := map[string]interface{}{ // Set your params
+				"<YOUR_QUERY_PARAM_KEY_1>": "<YOUR_QUERY_PARAM_VALUE_1>",
+				"<YOUR_QUERY_PARAM_KEY_2>": "<YOUR_QUERY_PARAM_VALUE_2>",
+			}
+			pathParams := map[string]string{"<YOUR_PATH_PARAM_KEY_1>": "<YOUR_PATH_PARAM_VALUE_1>"}
+			req := InvokeConnectionRequest{
+				Method:      POST, // set the request method
+				Headers:     headers,
+				Body:        body,
+				QueryParams: queryParams,
+				PathParams:  pathParams,
+			}
+			res, invokeError := service.Invoke(ctx, req)
+			if invokeError != nil {
+				fmt.Println("ERROR: ", *invokeError)
+			} else {
+				fmt.Println("RESPONSE", res)
+			}
+		}
 	}
 
 }
