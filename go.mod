dde6c484806a2d349a229c3ebf6174694d569cc3
Aadarsh <aadarsh.thakur@skyflow.com>
2026-05-27 15:13:02 +0530

    SK-2848: Removed deprectaed from other files

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-// Deprecated: skyflow-go v1 is deprecated and will reach End of Life on October 31, 2026. Please migrate to v2: https://github.com/skyflowapi/skyflow-go/blob/main/docs/migrate_to_v2.md
+// Deprecated: skyflow-go v1.x is deprecated and will reach End of Life on October 31, 2026. Please migrate to v2: https://github.com/skyflowapi/skyflow-go/blob/main/docs/migrate_to_v2.md
 module github.com/skyflowapi/skyflow-go
 
 go 1.18
--- skyflow/client/init.go
+++ skyflow/client/init.go
@@ -11,8 +11,6 @@ import (
 	"github.com/skyflowapi/skyflow-go/skyflow/common"
 )
 
-// Deprecated: Init is part of skyflow-go v1 which is deprecated and will reach End of Life on October 31, 2026.
-// Migrate to v2: https://github.com/skyflowapi/skyflow-go/blob/main/docs/migrate_to_v2.md
 func Init(configuration common.Configuration) Client {
 	logger.Info(fmt.Sprintf(messages.INITIALIZING_SKYFLOW_CLIENT, clientTag))
 	return Client{configuration}
